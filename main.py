24d90de2e94d0f92b8a2518a26acacacec44c896
Marcus-Gustafsson <marcus.thomas.gustafsson@hotmail.com>
2024-09-01 12:52:37 +0200

    Added drawing of player and rotation functionality

--- main.py
+++ main.py
@@ -2,8 +2,10 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+import player
 
 from constants import *
+
 
 def main():
     print("Starting asteroids!")
@@ -12,19 +14,31 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    dt = 0 # Delta
+    player_one = player.Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
 
     keep_going = True
 
     while keep_going:
+
         screen.fill((0,0,0))
-        pygame.display.flip()
+        player_one.draw(screen)
+
+        player_one.update(dt)
+
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
 
 
+        pygame.display.flip()
 
+
+        dt = (game_clock.tick(60)/1000) # 60 fps (waits a 1/60th second before updating), "1000" --> 1000 ms
 
 
 if __name__ == "__main__":
